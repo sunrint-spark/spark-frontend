850853a122ac5cf8b1a476de503f70e73905fd24
Yuchan Han <serltretu24@gmail.com>
2024-07-29 12:47:28 +0900

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/Home"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page</p>
+    </div>
+  );
+}
+
+export default HomePage;
