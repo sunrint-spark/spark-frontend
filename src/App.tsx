5decd6a8b5736eee44fd39bddc382af6a456b769
officeshinyujun <officeshinyujun.com>
2024-07-30 15:13:06 +0900

    ideaboard start

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/Home"
 import Community from "./pages/community.tsx";
+import Ideaboard from "./pages/ideaboard.tsx";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
           <Route path="/community" element={<Community/>}/>
+          <Route path ="/ideaboard" element={<Ideaboard/>}/>
       </Routes>
     </BrowserRouter>
   )
