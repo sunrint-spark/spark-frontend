cc753f0692eb7e64757cfe29514cf9d6857cc264
Yuchan Han <serltretu24@gmail.com>
2024-08-02 13:38:14 +0900

    feat: ResultDefaultNode 커스텀 노드에 추가
    사용자가 context-menu로 결과값을 보고 싶은 경우 관련 내용을 찾아줄 때 나오는 노드

---
+++ src/flow/nodes/ResultDefaultNode.tsx
@@ -0,0 +1,17 @@
+import {ItemNodeProps} from "@/types/ItemNodeProps"
+import {Handle, Position} from '@xyflow/react'
+import style from "@/styles/flownode.module.scss"
+
+
+export function ResultDefaultNode({data}: ItemNodeProps) {
+    return (
+        <>
+            {data.selected && <Handle type="target" position={Position.Right}/>}
+            <div className={data.selected ? style.selectItemNode : style.itemNode}>
+                <p className={style.ItemText}>
+                    {data.label}
+                </p>
+            </div>
+        </>
+    )
+}
