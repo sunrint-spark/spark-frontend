7c16fa7dba897b61314bbcf185c57f760557d22d
Yuchan Han <serltretu24@gmail.com>
2024-08-03 23:02:43 +0900

    feat(types): ItemNodeProps

--- src/flow/nodes/ItemNode.tsx
+++ src/flow/nodes/ItemNode.tsx
@@ -1,4 +1,4 @@
-import {ItemNodeProps} from "@/types/ItemNodeProps"
+import {ItemNodeProps} from "@/types/flownode"
 import {Handle, Position} from '@xyflow/react'
 import style from "@/styles/flownode.module.scss"
 
@@ -6,8 +6,8 @@ import style from "@/styles/flownode.module.scss"
 export function ItemNode({data}: ItemNodeProps) {
     return (
         <>
-            <Handle type="source" position={Position.Left}/>
-            {data.selected && <Handle type="target" position={Position.Right}/>}
+            <Handle type="target" position={Position.Left}/>
+            {data.selected && <Handle type="source" position={Position.Right}/>}
             <div className={data.selected ? style.selectItemNode : style.itemNode}>
                 <p className={style.ItemText}>
                     {data.label}
