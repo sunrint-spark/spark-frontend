75380536358d036e3de6cca35d7ca7416ed36241
Yuchan Han <serltretu24@gmail.com>
2024-08-05 02:31:28 +0900

    refactor: flow provider로 감싸기

--- src/pages/Brainstorm.tsx
+++ src/pages/Brainstorm.tsx
@@ -1,9 +1,12 @@
+import { ReactFlowProvider } from '@xyflow/react'
 import FlowApp from "../flow"
 
 function Brainstorm() {
     return (
         <div style={{width: '100vw', height: '100vh'}}>
-            <FlowApp />
+            <ReactFlowProvider>
+                <FlowApp />
+            </ReactFlowProvider>
         </div>
     );
 }
