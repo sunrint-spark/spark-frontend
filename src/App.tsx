8cd736743dd28c2efa859c91b5327c4676a21d33
Yuchan Han <serltretu24@gmail.com>
2024-08-15 21:58:56 +0900

    fix: merge router (ideaboard, brainstorm)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Community from "./pages/community";
 import { ModalProvider } from './modal/ModalContext';
-import Ideaboard from "./pages/ideaboard";
 import Brainstorm from "./pages/Brainstorm";
 import HomePage from "@/pages/Home.tsx";
 import './styles/global.css'
 import AuthRequired from "@/pages/AuthRequired";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 import { AuthWrapper } from "@/pages/AuthWrapper"
 
 const clientId = '975166081717-5kv5d2vbfj9nubq6jbo9u4u34b8lc0ir.apps.googleusercontent.com';
@@ -32,20 +32,16 @@ export default function App() {
                     <Community/>
               </AuthWrapper>
           }/>
-          <Route path ="/ideaboard" element={
-              <AuthWrapper>
-                  <ModalProvider>
-                      <Ideaboard/>
-                  </ModalProvider>
-              </AuthWrapper>
-          }/>
           <Route path="/brainstorm/:flowId" element={
               <AuthWrapper>
-                  <Brainstorm/>
+                  <ModalProvider>
+                      <Brainstorm/>
+                  </ModalProvider>
               </AuthWrapper>
           }/>
       </Routes>
     </BrowserRouter>
+          <Toaster/>
         </ThemeProvider>
   )
 }
