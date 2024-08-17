a2c9d4d688ea8ea4f44c8c9e142c9f80372234ac
Yuchan Han <serltretu24@gmail.com>
2024-08-18 01:48:58 +0900

    feat: context-menu에 결과값 보기 메뉴 추가

--- src/flow/nodes/ItemNode.tsx
+++ src/flow/nodes/ItemNode.tsx
@@ -4,70 +4,128 @@ import style from "@/styles/flownode.module.scss"
 import useLBRealtimeStore from "@/context/store.ts";
 import Api from "@/lib/api";
 import {generateRandomCode} from "@/lib/tree.ts";
+import {
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuLabel,
+    ContextMenuRadioGroup,
+    ContextMenuRadioItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
+} from "@/components/ui/context-menu"
 
 
 export function ItemNode({id, data}: ItemNodeProps) {
     const { nodes, setNodes, edges, setEdges } = useLBRealtimeStore();
     const thisNode = useNodes().find(n => n.id === id) as Node;
 
-    const handleClick = async () => {
+    const handleClick = async (command: string) => {
         data.selected = true
         Api.setToken(localStorage.getItem('token') as string)
         const response = await Api.streamAIBrainstorm(
-            data.sourceThreadId, data.label as string
+            data.sourceThreadId, command
         )
-        console.log(`[AIStartupNode] threadSelect: ${data.sourceThreadId}, select:${data.label}`)
+        console.log(`[AIStartupNode] threadSelect: ${data.sourceThreadId}, command: ${command}`)
         const result = response.result as Record<string, unknown>
-        const resultCategoryData = result.subcategories as string[]
-
-        const {x: sourceX, y: sourceY} = thisNode.position
-        const newNodes = resultCategoryData.map((category: string, index: number): Node => {
-            const startY = sourceY + (100 * index);
-            console.log(index)
-            return {
+        if (result.status === "select") {
+            const resultCategoryData = result.subcategories as string[]
+            const {x: sourceX, y: sourceY} = thisNode.position
+            const newNodes = resultCategoryData.map((category: string, index: number): Node => {
+                const startY = sourceY + (100 * index);
+                console.log(index)
+                return {
+                    id: generateRandomCode() as string,
+                    type: 'itemNode',
+                    data: {
+                        label: category,
+                        sourceThreadId: data.sourceThreadId,
+                        selected: false,
+                    },
+                    position: {
+                        y: startY,
+                        x: sourceX+300,
+                    },
+                }
+            })
+            const updateNodeData = (
+                nodeId: string,
+            ): void => {
+                const updatedNodes = nodes.map((node) =>
+                    node.id === nodeId ? { ...node, data: {
+                            ...node.data, selected: true
+                        } } : node
+                );
+                setNodes([...nodes, ...newNodes, ...updatedNodes]);
+            };
+            updateNodeData(id);
+            const newEdges = newNodes.map((node: Node) => {
+                return {
+                    id: generateRandomCode() as string,
+                    source: id,
+                    target: node.id,
+                }
+            })
+            setEdges([...edges, ...newEdges])
+        } else if (result.status === "end") {
+            console.log(`[AIStartupNode] end: ${result}`)
+            const newNode: Node = {
                 id: generateRandomCode() as string,
-                type: 'itemNode',
+                type: 'resultDefaultNode',
                 data: {
-                    label: category,
-                    sourceThreadId: data.sourceThreadId,
-                    selected: false,
+                    title: result.title as string,
+                    description: result.idea as string[],
+                    options: result.search_keyword as string[],
+                    optionsUrl: result.search_urls as string[],
                 },
                 position: {
-                    y: startY,
-                    x: sourceX+300,
+                    y: thisNode.position.y,
+                    x: thisNode.position.x+300,
                 },
             }
-        })
-        const updateNodeData = (
-            nodeId: string,
-        ): void => {
-            const updatedNodes = nodes.map((node) =>
-                node.id === nodeId ? { ...node, data: {
-                        ...node.data, selected: true
-                    } } : node
-            );
-            setNodes([...nodes, ...newNodes, ...updatedNodes]);
-        };
-        updateNodeData(id);
-        const newEdges = newNodes.map((node: Node) => {
-            return {
+            setNodes([...nodes, newNode])
+            const newEdge = {
                 id: generateRandomCode() as string,
                 source: id,
-                target: node.id,
+                target: newNode.id,
             }
-        })
-        setEdges([...edges, ...newEdges])
+            setEdges([...edges, newEdge])
+        }
     }
 
     return (
-        <div onClick={handleClick}>
-            <Handle type="target" position={Position.Left}/>
-            {data.selected && <Handle type="source" position={Position.Right}/>}
-            <div className={data.selected ? style.selectItemNode : style.itemNode}>
-                <p className={style.ItemText}>
-                    {data.label}
-                </p>
+        <ContextMenu>
+            <div onClick={() => {
+                handleClick(data.label as string)
+            }}>
+                <ContextMenuTrigger>
+                    <Handle type="target" position={Position.Left}/>
+                    {data.selected && <Handle type="source" position={Position.Right}/>}
+                    <div className={data.selected ? style.selectItemNode : style.itemNode}>
+                        <p className={style.ItemText}>
+                            {data.label}
+                        </p>
+                    </div>
+                </ContextMenuTrigger>
+                <ContextMenuContent className="w-64">
+                    <ContextMenuItem inset onClick={() => {
+                        handleClick(data.label as string)
+                    }}>
+                        선택
+                        <ContextMenuShortcut>[Left Click]</ContextMenuShortcut>
+                    </ContextMenuItem>
+                    <ContextMenuItem inset onClick={() => {
+                        handleClick(`#end ${data.label}`)
+                    }}>
+                        여기서 결과값 보기
+                    </ContextMenuItem>
+                </ContextMenuContent>
             </div>
-        </div>
+        </ContextMenu>
     )
 }
