b1c4aca74b4d27921b6c4377165f1d7ba2972f54
Yuchan Han <serltretu24@gmail.com>
2024-08-21 09:55:11 +0900

    feat: memoNode 수정 기능 추가

--- src/flow/nodes/MemoNode.tsx
+++ src/flow/nodes/MemoNode.tsx
@@ -1,15 +1,31 @@
 import {MemoNodeProps} from "@/types/flownode"
+import {useState} from "react";
 import {Handle, Position} from '@xyflow/react'
 import style from "@/styles/flownode.module.scss"
+import {Node, useNodes} from '@xyflow/react';
+import useLBRealtimeStore from "@/context/store.ts";
 
 
-export function MemoNode({data}: MemoNodeProps) {
+export function MemoNode({id, data}: MemoNodeProps) {
+    const { nodes, setNodes } = useLBRealtimeStore();
+    const thisNode = nodes.find(n => n.id === id) as Node;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_text, setText] = useState(thisNode.data.text as string);
+
     return (
         <>
-            <Handle type="source" position={Position.Top}/>
             <div className={style.memoNode}>
                 <p className={style.memoText}>
-                    {data.label}
+                    <textarea placeholder="여기에 메모 적기" value={data.text} onChange={
+                        (e) => {
+                            setText(e.target.value)
+                            setNodes(nodes.map((node) =>
+                                node.id === id ? { ...node, data: {
+                                        ...node.data, text: e.target.value
+                                    } } : node
+                            ))
+                        }
+                    }/>
                 </p>
                 <p className={style.memoTextAuthor}>
                     {data.authorName}
